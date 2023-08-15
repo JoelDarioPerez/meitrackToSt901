12f3d192ba2160481c7a5c020167233d35b3f6e5
Joel Perez <perezjoeldario@gmail.com>
2023-08-15 11:03:18 -0300

    a

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const net = require('net');
 
+// Dirección IP y puerto del servidor al que reenviar los datos
+const remoteServerHost = 'hwc9760.gpsog.com';
+const remoteServerPort = 9760;
+
 const server = net.createServer(socket => {
   console.log('Cliente conectado.');
 
+  // Crear una conexión con el servidor remoto
+  const remoteSocket = net.createConnection(remoteServerPort, remoteServerHost, () => {
+    console.log('Conectado al servidor remoto.');
+  });
+
   socket.on('data', data => {
-    const hexData = data.toString('hex');
-    console.log(`Datos recibidos en formato hexadecimal: ${hexData}`);
+    const decodedData = data.toString();
+    console.log(`Datos recibidos: ${decodedData}`);
+
+    // Reenviar los datos al servidor remoto
+    remoteSocket.write(data);
   });
 
   socket.on('end', () => {
     console.log('Cliente desconectado.');
+
+    // Cerrar la conexión con el servidor remoto cuando el cliente se desconecta
+    remoteSocket.end();
   });
 
   socket.on('error', error => {
