f09780c62c8612be3ecb90972f9bbf12b5847453
Joel Perez <perezjoeldario@gmail.com>
2023-08-17 11:22:33 -0300

    a

--- server.js
+++ server.js
@@ -3,20 +3,8 @@ const net = require('net');
 const server = net.createServer(socket => {
   console.log('Cliente conectado.');
 
-  let buffer = Buffer.alloc(0); // Buffer para almacenar los datos entrantes
-
   socket.on('data', data => {
-    buffer = Buffer.concat([buffer, data]); // Agregar datos al buffer
-
-    // Verificar si se ha recibido al menos 512 bytes
-    if (buffer.length >= 512) {
-      const receivedData = buffer.slice(0, 512);
-
-      console.log('Datos completos recibidos en formato HEX:', receivedData.toString('hex'));
-
-      // Borrar los datos procesados del buffer
-      buffer = buffer.slice(512);
-    }
+    console.log('Datos recibidos en formato HEX:', data.toString('hex'));
   });
 
   socket.on('end', () => {
